52d112e9892bca5603bb5aa9673cbed98d26d821
MaximoJunior <studentdeveloper19@gmail.com>
2020-10-25 18:51:16 -0400

    Updates

---
+++ precache-manifest.2dd237913f52f1df31156425572bf5e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e572b05eb029798cf351472939206122",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "2117e02690ae23f070e5",
+    "url": "/pomodoro/static/css/main.44531c85.chunk.css"
+  },
+  {
+    "revision": "56ab41e6f86936dc1533",
+    "url": "/pomodoro/static/js/2.29716c7c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro/static/js/2.29716c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2117e02690ae23f070e5",
+    "url": "/pomodoro/static/js/main.65aff7a9.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  }
+]);
